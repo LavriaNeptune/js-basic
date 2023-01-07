7d8333fa453711415a3e39c58d8a46b785fb3165
Lavria <fangsunedu@gmail.com>
2023-01-07 23:58:59 +0800

    update

---
+++ .history/function/arrow-function/main_20220707180246.js
@@ -0,0 +1,9 @@
+// 编写一个简单的箭头函数
+let sum = (a, b) => a + b;
+console.log(sum(6, 9));
+
+// 如果不使用箭头函数，而是使用普通函数：
+// let sum = function (a, b) {
+//   return a + b;
+// };
+// console.log(sum(6, 9));
---
+++ .history/function/arrow-function/main_20221215200042.js
@@ -0,0 +1,10 @@
+// 编写一个简单的箭头函数
+let sum = (a, b) => a + b;
+console.log(sum(6, 9));
+let github = 'SB';
+
+// 如果不使用箭头函数，而是使用普通函数：
+// let sum = function (a, b) {
+//   return a + b;
+// };
+// console.log(sum(6, 9));
